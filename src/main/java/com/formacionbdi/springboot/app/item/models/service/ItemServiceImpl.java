abc8756533a7db712c1ebec5067bd747eaad2b1e
gino <gino@gmail.com>
2020-03-27 21:03:26 -0500

    CRUD consumiendo APIs con Feign y Rest Template

--- src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
+++ src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
@@ -7,6 +7,9 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -39,6 +42,43 @@ public class ItemServiceImpl implements ItemService {
 			
 		Producto producto = clienteRest.getForObject("http://servicio-productos/ver/{id}", Producto.class,pathVariables);
 		return new Item(producto, cantidad);
+	}
+
+	@Override
+	public Producto save(Producto producto) {
+		
+		//(de argumento el objeto producto, que es lo que se quiere enviar)
+		HttpEntity<Producto> body = new HttpEntity<>(producto);
+		
+		/*(endpoint, tipo de la peticion, enviar el reequest o body que contiene el objeto producto,
+		   el tipo de dato en el que se quiere recibir el dato el JSON el APIrest que retorna)*/
+		ResponseEntity<Producto> response = clienteRest.exchange("http://servicio-productos/crear", HttpMethod.POST, body, Producto.class);
+		/*getBody(), retorna el cuerpo de la respuesta que sera de tipo Producto porque se le indico en
+		 * los parametros de exchange*/
+		Producto productoResponse = response.getBody();
+		
+		return productoResponse;
+	}
+
+	@Override
+	public Producto update(Producto producto, Long id) {
+		Map<String, String> pathVariables = new HashMap<String, String>();
+		pathVariables.put("id", id.toString());
+		
+		HttpEntity<Producto> body = new HttpEntity<>(producto);
+		ResponseEntity<Producto> response = clienteRest.exchange("http://servicio-productos/editar/{id}", 
+				HttpMethod.PUT, body, Producto.class, pathVariables);
+		
+		return response.getBody();
+	}
+
+	@Override
+	public void delete(Long id) {
+		Map<String, String> pathVariables = new HashMap<String, String>();
+		pathVariables.put("id", id.toString());
+		
+		clienteRest.delete("http://servicio-productos/eliminar/{id}",pathVariables);
+		
 	}
 
 }
